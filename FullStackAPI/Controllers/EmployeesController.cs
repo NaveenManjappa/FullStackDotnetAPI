0bdc182ff33eebc133aae17052bb7ec22913e5b1
naveenkengannar <naveenkmforums@gmail.com>
2023-12-17 16:09:09 +0000

    Added APIs for getting employees

---
+++ FullStackAPI/Controllers/EmployeesController.cs
@@ -0,0 +1,35 @@
+﻿using FullStackAPI.Data;
+using FullStackAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FullStackAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmployeesController : Controller
+    {
+        private readonly FullStackDbContext _fullStackDbContext;
+        public EmployeesController(FullStackDbContext fullStackDbContext)
+        {
+            _fullStackDbContext = fullStackDbContext;
+        }
+        [HttpGet]
+        public async Task<ActionResult> GetAllEmployees()
+        {
+           var employees= await _fullStackDbContext.Employees.ToListAsync();
+            return Ok(employees);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddEmployee([FromBody]Employee empRequest)
+        {
+            Random random = new Random();
+
+            //empRequest.Id = random.Next();
+            await _fullStackDbContext.Employees.AddAsync(empRequest);
+            await _fullStackDbContext.SaveChangesAsync();
+            return Ok(empRequest);
+        }
+    }
+}
