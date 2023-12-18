dac5d5d3391446436f184023d08f1c707e291d11
naveenkengannar <naveenkmforums@gmail.com>
2023-12-18 07:20:50 +0000

    Added edit and delete apis

--- FullStackAPI/Controllers/EmployeesController.cs
+++ FullStackAPI/Controllers/EmployeesController.cs
@@ -2,6 +2,7 @@
 using FullStackAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Runtime.InteropServices;
 
 namespace FullStackAPI.Controllers
 {
@@ -30,6 +31,50 @@ namespace FullStackAPI.Controllers
             await _fullStackDbContext.Employees.AddAsync(empRequest);
             await _fullStackDbContext.SaveChangesAsync();
             return Ok(empRequest);
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public async Task<IActionResult> GetEmployee([FromRoute] int id)
+        {
+            var emp = await _fullStackDbContext.Employees.FirstOrDefaultAsync(e => e.Id == id);
+            if (emp == null)
+                return NotFound();
+
+            return Ok(emp);
+
+        }
+
+        [HttpPut]
+        [Route("{id:int}")]
+        public async Task<IActionResult> UpdateEmployee([FromRoute] int id,Employee employee)
+        {
+           var emp= await _fullStackDbContext.Employees.FindAsync(id);
+            if(emp == null)
+                return NotFound();
+
+            emp.Name = employee.Name;
+            emp.Email = employee.Email;
+            emp.Phone = employee.Phone;
+            emp.Salary = employee.Salary;
+            emp.Department = employee.Department;
+
+            await _fullStackDbContext.SaveChangesAsync();
+            return Ok(emp);
+            
+        }
+
+        [HttpDelete]
+        [Route("{id:int}")]
+        public async Task<IActionResult> DeleteEmployee([FromRoute] int id)
+        {
+            var emp = await _fullStackDbContext.Employees.FindAsync(id);
+            if (emp == null)
+                return NotFound();
+
+            _fullStackDbContext.Employees.Remove(emp);
+            await _fullStackDbContext.SaveChangesAsync();
+            return Ok(emp);
         }
     }
 }
